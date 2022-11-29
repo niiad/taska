57f1b0cf2ab4e685ae8be46679c0432727ecf4d3
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-11-29 13:53:09 +0000

    Update App.tsx

--- taska/src/App.tsx
+++ taska/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>("");
+
+
+
   return (
     <div className="App">
       <span className="heading">Taska</span>
-        <InputField/>
+        <InputField todo={task} setTodo={setTask()}/>
     </div>
   );
 }
