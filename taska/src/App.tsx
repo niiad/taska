d34eaaefa7be919a8c038cff68507a76d4d77738
Nii Adotei <61387678+Nii-Adotei@users.noreply.github.com>
2022-11-29 14:18:07 +0000

    Update App.tsx

--- taska/src/App.tsx
+++ taska/src/App.tsx
@@ -1,16 +1,18 @@
 import React, {useState} from 'react';
 import './App.css';
 import InputField from "./components/InputField";
+import {Task} from "./entities/Task";
 
 const App: React.FC = () => {
   const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Task[]>([]);
 
-
+  const onTaskAdd = () => {};
 
   return (
     <div className="App">
       <span className="heading">Taska</span>
-        <InputField task={task} setTask={setTask}/>
+        <InputField task={task} setTask={setTask} onTaskAdd={onTaskAdd()}/>
     </div>
   );
 }
